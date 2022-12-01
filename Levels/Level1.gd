fbe2e182ec84eeeff8f95d22c6b6c768f7c75d87
Colin Hans <cjhans@iu.edu>
2022-12-01 17:18:41 -0500

    fixed errors

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -11,6 +11,8 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
-	if not $Enemy and not $Enemy2:
-		if $Door:
-			$Door.queue_free()
+	var Door = get_node_or_null("/root/Level1/Door")
+	if Door != null:
+		var Enemy_Container = get_node_or_null("/root/Level1/Enemy_Container")
+		if Enemy_Container != null and Enemy_Container.get_child_count() == 0:
+			Door.queue_free()
